2a5aa3c03d9fef500e23c835a248fc0a2cc6ec46
Yasir Khan <mykhan440@outlook.com>
2024-11-23 02:50:10 +0300

    Update __manifest__.py

--- bi_print_journal_entries/__manifest__.py
+++ bi_print_journal_entries/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Print Journal Entries Report in Odoo',
-    'version': '17.0.0.1',
+    'version': '17.0.0.2',
     'category': 'Account',
     'license': 'OPL-1',
     'summary': 'Allow to print pdf report of Journal Entries.',
